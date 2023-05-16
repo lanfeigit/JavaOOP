75ea5b5bc436f3e2b55395052d99d59f38ca1992
lanfeica <132637043+lanfeica@users.noreply.github.com>
2023-05-16 18:41:12 -0400

    updated test class

--- EmployeePay/src/programmer/PaymeInterfaceTest.java
+++ EmployeePay/src/programmer/PaymeInterfaceTest.java
@@ -7,8 +7,7 @@
  */
 package programmer;
 
-
-/** 
+/**
  * 
  * This is the test for your interface Payme.
  * 
@@ -26,33 +25,39 @@ public class PaymeInterfaceTest {
 		paymeObjects[2] = new SalariedProgrammer("Chioma", "Chidmmia", "345-67-0001", 10, 2022, 320.0);
 		paymeObjects[3] = new HourlyProgrammer("Amara", "Chukwu", "234-56-7770", 10, 2022, 18.95, 40.0);
 		paymeObjects[4] = new CommissionProgrammer("Peter", "Goodman", "123-45-6999", 10, 2022, 16500.0, 0.44);
-		paymeObjects[5] = new BasePlusCommissionProgrammer("Esther", "Patel", "102-34-5888", 10, 2022, 1200.0, 0.04, 720.0);
+		paymeObjects[5] = new BasePlusCommissionProgrammer("Esther", "Patel", "102-34-5888", 10, 2022, 1200.0, 0.04,
+				720.0);
 
 		System.out.println("Payment for Invoices and Programmers are processed polymorphically:\n");
 
-		// generically process each element in array paymeObjects
-		for (Payme currentPayme : paymeObjects) {
-			// output currentPayme and its appropriate payment amount
-			System.out.printf("%s \n", currentPayme.toString());
-
-			if (currentPayme instanceof BasePlusCommissionProgrammer) {
-				// downcast Payme reference to
-				// BasePlusCommissioProgrammer reference
-				BasePlusCommissionProgrammer programmer = (BasePlusCommissionProgrammer) currentPayme;
-
-				double oldBaseSalary = programmer.getBaseSalary();
-				programmer.setBaseSalary(1.10 * oldBaseSalary);
-				System.out.printf("new base salary with 10%% increase is: $%,.2f\n", programmer.getBaseSalary());
-			}
-
-  	        System.out.printf("%s: $%,.2f\n\n", "payment due", currentPayme.getPaymentAmount());
-		}
-		
 		try {
-		
-			Programmer programmer = (Programmer) paymeObjects[3];		
+			// generically process each element in array paymeObjects
+			for (Payme currentPayme : paymeObjects) {
+				// output currentPayme and its appropriate payment amount
+				System.out.printf("%s \n", currentPayme.toString());
+
+				if (currentPayme instanceof BasePlusCommissionProgrammer) {
+					// downcast Payme reference to
+					// BasePlusCommissioProgrammer reference
+					BasePlusCommissionProgrammer programmer = (BasePlusCommissionProgrammer) currentPayme;
+
+					double oldBaseSalary = programmer.getBaseSalary();
+					programmer.setBaseSalary(1.10 * oldBaseSalary);
+					System.out.printf("new base salary with 10%% increase is: $%,.2f\n", programmer.getBaseSalary());
+				}
+
+				System.out.printf("%s: $%,.2f\n\n", "payment due", currentPayme.getPaymentAmount());
+			}
+		} catch (IllegalArgumentException ex) {
+			System.out.printf("Error occured with bad argument: ");
+			ex.printStackTrace();
+		}
+
+		try {
+
+			Programmer programmer = (Programmer) paymeObjects[3];
 			programmer.isJavaFun(false);
-			
+
 		} catch (IllegalArgumentException ex) {
 			System.out.println(ex.getMessage());
 		}
